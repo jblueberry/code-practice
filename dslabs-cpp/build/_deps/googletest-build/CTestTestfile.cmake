69209ae87f4f0cf614dce9d5df79962fe20ff5e0
junhui <daniel.zhujunhui@gmail.com>
2023-04-26 19:32:32 +0800

    remove build

--- dslabs-cpp/build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/junhui/code-practice/dslabs-cpp/build/_deps/googletest-src
-# Build directory: /home/junhui/code-practice/dslabs-cpp/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- dslabs-cpp/build/test/gtest_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/junhui/code-practice/dslabs-cpp/build/test/gtest_test[1]_tests.cmake")
-  include("/home/junhui/code-practice/dslabs-cpp/build/test/gtest_test[1]_tests.cmake")
-else()
-  add_test(gtest_test_NOT_BUILT gtest_test_NOT_BUILT)
-endif()
